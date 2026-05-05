64f1331cd43e4be9bacce953523e38b44ef6c218
Eduard Castellano <educastellano08@gmail.com>
2026-05-05 15:53:40 +0200

    Add utility to write markers

--- test.js
+++ test.js
@@ -30,3 +30,19 @@ test('read markers', (t) => {
   t.ok(markers.every((marker) => Number.isInteger(marker.marker)))
   t.ok(markers.every((marker) => Buffer.isBuffer(marker.data)))
 })
+
+test('write markers', (t) => {
+  const image = require('./test/fixtures/grapefruit.jpg', {
+    with: { type: 'binary' }
+  })
+  const APP15 = 0xef
+  const data = Buffer.from('a marker')
+
+  const output = jpeg.writeMarkers(image, [{ marker: APP15, data }])
+
+  const markers = jpeg.readMarkers(output)
+  const marker = markers.find((marker) => marker.marker === APP15)
+
+  t.ok(Buffer.isBuffer(output))
+  t.ok(marker.data.equals(data))
+})
